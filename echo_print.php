b3bd231ae4ed5d7f48f9059fe9e99c8bec8e6e95
Andre <andreleecoder@gmail.com>
2019-07-03 17:09:49 -0300

    echo print

--- echo_print.php
+++ echo_print.php
@@ -4,15 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>curso php</title>
+    <title>echo print</title>
 </head>
 <body>
-    <?php 
-    echo 'utilizando a tag padrão';
-    ?>
-    <br/>
-    <?='utilizando a tag de impressão'?>
-    <br/>
-    <? echo 'utilizando a tag de curta'?>
+
+    <?php
+    echo ' comando echo <br/>';
+    print ' comando print <hr/>';
+   ?>
 </body>
 </html>
