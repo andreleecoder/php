b32da97e0c423c2caaa21905c281a08dcc367210
Andre <andreleecoder@gmail.com>
2019-07-04 17:44:09 -0300

    variaveis

---
+++ comentarios.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>comentarios</title>
+</head>
+<body>
+
+    <?php
+    //comentário de uma linha
+    echo ' comando echo <br/>';
+    #comentario de uma linha
+    print ' comando print <hr/>';
+    /* 
+    comentario de varias
+    esse tipo de comentario permite que varias
+     linhas sejam comentadas com varias linhas ao mesmo tempo
+    */
+   ?>
+</body>
+</html>
