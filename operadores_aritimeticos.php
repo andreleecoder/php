1fac680bb252454789b92ccd0f4156453626e992
Andre <andreleecoder@gmail.com>
2019-07-19 11:00:00 -0300

    operadores aritimeticos

--- operadores_aritimeticos.php
+++ operadores_aritimeticos.php
@@ -11,6 +11,32 @@
     <?php
       $num1=13;
       $num2=4;
+      echo 'A soma entre $num1 e $num2 é, '.($num1+$num2);
+      echo '<br/>';
+      echo 'A subtração entre $num1 e $num2 é, '.($num1-$num2);
+      echo '<br/>';
+      echo 'A multiplicação entre $num1 e $num2 é, '.($num1*$num2);
+      echo '<br/>';
+      echo 'A divisão entre $num1 e $num2 é, '.($num1/$num2);
+      echo '<br/>';
+      echo 'O módulo entre $num1 e $num2 é, '.($num1%$num2);
+      echo '<hr/>';
+      //operadores aritimeticos na atribuição de valores
+      //isso serve para todos os operadores aritimeticos
+      $x = 10;
+      $x += 5;
+      echo $x;
+      echo '<hr/>';
+      $y=8;
+      $y -=5;
+      echo $y;
+      echo '<hr/>';
+      //operadoers de incremento e decremento
+      $a=10;
+      //$a++;
+      $a--;
+      echo $a
+
 
    ?>
 
