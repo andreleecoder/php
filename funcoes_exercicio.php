2f8a0dbdcd1e598898ab90c2f23d091690346fc9
Andre <andreleecoder@gmail.com>
2019-07-22 17:47:11 -0300

    bye

--- funcoes_exercicio.php
+++ funcoes_exercicio.php
@@ -12,17 +12,17 @@
     function calcularImpostoDeRenda($salario){  
         if($salario<=1903.99){
             return"isento";
-        }elseif ($salario==1903.99||$salario<=2826.65) {
+        }elseif ($salario==1903.99&&$salario<=2826.65) {
             return "7.5%";
-        }elseif ($salario==2826.66||$salario<=3751.05) {
+        }elseif ($salario==2826.66&&$salario<=3751.05) {
             return "15%";
-        }elseif ($salario==3751.06||$salario<=4664.68) {
+        }elseif ($salario==3751.06&&$salario<=4664.68) {
             return "22.5%";
         }elseif ($salario>=4664.68) {
             return "27.5%";
          }
         }
-    echo calcularImpostoDeRenda(1900);
+    echo calcularImpostoDeRenda(5000);
    ?>
   
 </body>
