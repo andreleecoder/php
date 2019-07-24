192a06b54b986e346cde08d0ff316b0d13400731
Andre <andreleecoder@gmail.com>
2019-07-24 10:54:01 -0300

    datas php

---
+++ funcoes_datas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>funçoes datas</title>
+</head>
+<body>
+   
+    <?php
+        /*consultar a documentação do php para descobrir os tokens de recuperação de datas*/
+
+        //recuperação da data atual/data corrente
+        echo date('d').'<br/>';//dia do mes
+        echo date('D').'<br/>';//dia da semana
+        echo date('d/M/y H:i').'<br/>';//dia/mes/ano, horas mnutos
+
+    ?>
+  
+</body>
+</html>
--- funcoes_matematicas.php
+++ funcoes_matematicas.php
@@ -10,11 +10,18 @@
    
     <?php
     $num=7.3;
-
+    echo $num;
     echo ceil($num);//arredonda para cima
     echo '<hr/>';
     echo floor($num);//arredonda para baixo
-
+    echo '<hr/>';
+    echo round($num);//0 e .4-> arredonda p baixo/.5 e .9-> arredonda p cima
+    echo '<hr/>';
+    echo rand(10,20);//gera um valor aleatório de 0 até randmax
+    echo '<hr/>';
+    echo getrandmax();
+    echo '<hr/>';
+    echo sqrt(20);//retorna a raiz quadrada de um numero
     ?>
   
 </body>
