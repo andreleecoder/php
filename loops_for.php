8b212c58881289b30b2b8d95766afc30eb48243e
Andre <andreleecoder@gmail.com>
2019-08-08 14:22:11 -0300

    for loop

---
+++ loops_for.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>loops for </title>
+</head>
+<body>
+   
+    <?php
+    /*$x=10;
+    do {
+        echo'Entrou no do while';
+        //continue e break podem ser usados aqui
+    } while ($x < 9);
+    echo'<hr/>';
+    while($x > 9){
+        echo'Entrou no while';
+        break;
+
+    }*/
+    
+  
+    ?>
+  
+</body>
+</html>
