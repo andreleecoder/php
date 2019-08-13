b292a896af1a352506c166195cebb2760f301d83
Andre <andreleecoder@gmail.com>
2019-08-13 17:18:15 -0300

    nothing

---
+++ loops exercicios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exercício php </title>
+</head>
+<body>
+   crie um script capaz de produzir, atravé de um loop,um array com 
+   6 elementos numericos aleatórios entre 1 e 60, simulando o funcionamento 
+   do sorteio da mega sena, atente-se ao fato de que os numeros aleatorios 
+   contidos dentro do array nao podem ser repetidos.
+
+   inicio programa
+    criar array de 6 elementos
+    gerar numeros de forma aleatória
+    imprimir resultado na tela 
+    atualizar pagina 
+    gerar mais numeros aleatórios
+
+    <?php
+    
+    ?>
+  
+</body>
+</html>
